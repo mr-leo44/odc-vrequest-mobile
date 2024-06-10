6683933ef0f79b5337c45012499a0b2342a6cb22
salomon <salomonluveto2023@gmail.com>
2024-06-10 15:47:28 +0100

    refactor : changement des importations dans userinteractor

--- lib/m_user/business/interactor/UserInteractor.dart
+++ lib/m_user/business/interactor/UserInteractor.dart
@@ -1,15 +1,17 @@
-import 'package:odc_mobile_project/m_user/business/interactor/userUseCase/DisconnectUseCase.dart';
-import 'package:odc_mobile_project/m_user/business/interactor/userUseCase/GetTokenUseCase.dart';
-import 'package:odc_mobile_project/m_user/business/interactor/userUseCase/GetUserLocalUseCase.dart';
-import 'package:odc_mobile_project/m_user/business/interactor/userUseCase/GetUserNetworkUseCase.dart';
-import 'package:odc_mobile_project/m_user/business/interactor/userUseCase/SaveTokenUseCase.dart';
-import 'package:odc_mobile_project/m_user/business/interactor/userUseCase/SaveUserUseCase.dart';
+
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../service/userLocalService.dart';
 import '../service/userNetworkService.dart';
-import 'userUseCase/AuthenticateUseCase.dart';
+import 'user/AuthenticateUseCase.dart';
+import 'user/DisconnectUseCase.dart';
+import 'user/GetTokenUseCase.dart';
+import 'user/GetUserLocalUseCase.dart';
+import 'user/GetUserNetworkUseCase.dart';
+import 'user/SaveTokenUseCase.dart';
+import 'user/SaveUserUseCase.dart';
+
 part "UserInteractor.g.dart";
 
 class UserInteractor{
