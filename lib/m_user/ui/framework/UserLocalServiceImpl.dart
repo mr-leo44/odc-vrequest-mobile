ca6918b19657a2db4a957b8d33ff5585161386df
salomon <salomonluveto2023@gmail.com>
2024-06-10 16:47:12 +0100

    création des implémentations

--- lib/m_user/ui/framework/UserLocalServiceImpl.dart
+++ lib/m_user/ui/framework/UserLocalServiceImpl.dart
@@ -1,5 +1,6 @@
 import 'package:get_storage/get_storage.dart';
 import 'package:odc_mobile_project/m_user/business/model/AuthenticateResponse.dart';
+import 'package:odc_mobile_project/m_user/business/model/User.dart';
 
 import '../../business/service/userLocalService.dart';
 
@@ -15,11 +16,7 @@ class UserLocalServiceImpl implements UserLocalService{
    return Future.value(data);
   }
 
-  @override
-  Future<AuthenticateResponse> getUser() {
-    var data= stockage.read("USER")?? {};
-    return Future.value(AuthenticateResponse.fromJson(data));
-  }
+
 
   @override
   Future<bool> saveToken(String data) async {
@@ -27,10 +24,24 @@ class UserLocalServiceImpl implements UserLocalService{
     return true;
   }
 
+ 
+
   @override
-  Future<bool> saveUser(AuthenticateResponse data) async{
-    await stockage.write("USER", data.toJson());
-    return true;
+  Future disconnect() {
+    // TODO: implement disconnect
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User?> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> saveUser(User data) {
+    // TODO: implement saveUser
+    throw UnimplementedError();
   }
 
 }
