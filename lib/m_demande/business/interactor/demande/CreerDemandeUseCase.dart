96ed1748f4d495df0b19ae2c22c78e303743b881
Foster Mbayi <145870975+77foster@users.noreply.github.com>
2024-06-11 13:29:44 +0100

    Integration du UserLocalService dans les uses cases

--- lib/m_demande/business/interactor/demande/CreerDemandeUseCase.dart
+++ lib/m_demande/business/interactor/demande/CreerDemandeUseCase.dart
@@ -1,17 +1,18 @@
-import 'package:odc_mobile_project/m_demande/business/model/Site.dart';
+import 'package:odc_mobile_project/m_demande/business/model/DemandeRequest.dart';
 import 'package:odc_mobile_project/m_demande/business/service/DemandeLocalService.dart';
 import 'package:odc_mobile_project/m_demande/business/service/DemandeNetworkService.dart';
 import 'package:odc_mobile_project/m_user/business/service/userLocalService.dart';
 
-class ListSiteUseCase {
+class CreerDemandeUseCase {
   DemandeNetworkService network;
   UserLocalService userLocal;
 
-  ListSiteUseCase(this.network, this.userLocal);
+  CreerDemandeUseCase(this.network, this.userLocal);
 
-  Future<List<Site?>> run() async {
+  Future<String?> run(DemandeRequest data) async {
     String token = await userLocal.getToken();
-    var res = await network.listSite(token);
+    var res = await network.creerDemande(data, token);
+
     return res;
   }
 }
--- lib/m_demande/business/interactor/demande/ListeDemandeUseCase.dart
+++ lib/m_demande/business/interactor/demande/ListeDemandeUseCase.dart
@@ -1,17 +1,18 @@
-import 'package:odc_mobile_project/m_demande/business/model/Site.dart';
+import 'package:odc_mobile_project/m_demande/business/model/Demande.dart';
 import 'package:odc_mobile_project/m_demande/business/service/DemandeLocalService.dart';
 import 'package:odc_mobile_project/m_demande/business/service/DemandeNetworkService.dart';
-import 'package:odc_mobile_project/m_user/business/service/userLocalService.dart';
 
-class ListSiteUseCase {
+import '../../../../m_user/business/service/userLocalService.dart';
+
+class ListDemandeUseCase {
   DemandeNetworkService network;
   UserLocalService userLocal;
 
-  ListSiteUseCase(this.network, this.userLocal);
+  ListDemandeUseCase(this.network, this.userLocal);
 
-  Future<List<Site?>> run() async {
+  Future<List<Demande?>> run() async {
     String token = await userLocal.getToken();
-    var res = await network.listSite(token);
+    var res = await network.listDemande(token);
     return res;
   }
 }
