3a472927f59b0ecad801620eef9fe68afb7699c7
bapmutembs <bapmutembs@gmail.com>
2024-06-18 17:37:53 +0100

    mettre l'attribut avatar optionnel dans ChatUsersModel

--- lib/m_chat/ui/pages/ChatList/ChatUsersModel.dart
+++ lib/m_chat/ui/pages/ChatList/ChatUsersModel.dart
@@ -1,21 +1,43 @@
 class ChatUsersModel {
-  String avatar;
-  String ticket;
-  String lastSender;
-  String lastMessage;
-  bool isVideo;
-  bool isMessageRead;
-  String time;
-  int unread;
+    String avatar;
+    String ticket;
+    String lastSender;
+    String lastMessage;
+    bool isVideo;
+    bool isMessageRead;
+    String time;
+    int unread;
 
-  ChatUsersModel({
-    this.avatar = "assets/images/avatar.jpeg" ,
-    required this.ticket,
-    required this.lastSender,
-    required this.lastMessage,
-    required this.isVideo,
-    required this.isMessageRead,
-    required this.time,
-    required this.unread,
-  });
+    ChatUsersModel({
+        required this.avatar,
+        required this.ticket,
+        required this.lastSender,
+        required this.lastMessage,
+        required this.isVideo,
+        required this.isMessageRead,
+        required this.time,
+        required this.unread,
+    });
+
+    ChatUsersModel copyWith({
+        String? avatar,
+        String? ticket,
+        String? lastSender,
+        String? lastMessage,
+        bool? isVideo,
+        bool? isMessageRead,
+        String? time,
+        int? unread,
+    }) => 
+        ChatUsersModel(
+            avatar: avatar ?? "assets/images/avatar.jpeg",
+            ticket: ticket ?? this.ticket,
+            lastSender: lastSender ?? this.lastSender,
+            lastMessage: lastMessage ?? this.lastMessage,
+            isVideo: isVideo ?? this.isVideo,
+            isMessageRead: isMessageRead ?? this.isMessageRead,
+            time: time ?? this.time,
+            unread: unread ?? this.unread,
+        );
 }
+
