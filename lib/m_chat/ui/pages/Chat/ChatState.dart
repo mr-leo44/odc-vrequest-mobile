c3cbe8dd9ceb315aa91e2e6955638f4d6d7d4ee1
bapmutembs <bapmutembs@gmail.com>
2024-06-21 18:17:56 +0100

    suppression des proprietes indesirables de la classe

--- lib/m_chat/ui/pages/Chat/ChatState.dart
+++ lib/m_chat/ui/pages/Chat/ChatState.dart
@@ -1,25 +1,21 @@
-import 'package:odc_mobile_project/m_chat/ui/pages/Chat/ChatModel.dart';
-import 'package:odc_mobile_project/m_chat/ui/pages/ChatList/ChatUsersModel.dart';
+import 'package:odc_mobile_project/m_chat/business/model/ChatModel.dart';
+import 'package:odc_mobile_project/m_chat/business/model/ChatUsersModel.dart';
 
 class ChatState {
   bool isLoading;
   List<ChatModel> chatList = [];
-  ChatUsersModel chatUsers ;
 
   ChatState({
     this.isLoading = false,
     this.chatList = const [],
-    required this.chatUsers,
   });
 
   ChatState copyWith({
     bool? isLoading,
     List<ChatModel>? chatList,
-    required ChatUsersModel chatUsers
   }) =>
       ChatState(
         isLoading: isLoading ?? this.isLoading,
         chatList: chatList ?? this.chatList,
-        chatUsers: chatUsers,
       );
 }
