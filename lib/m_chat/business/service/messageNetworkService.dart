e1b06c07333c1785365f77381171ba66df0fd69f
bapmutembs <bapmutembs@gmail.com>
2024-06-21 18:14:50 +0100

    rectifier les type et les parametres des methodes de l'abstraction du service

--- lib/m_chat/business/service/messageNetworkService.dart
+++ lib/m_chat/business/service/messageNetworkService.dart
@@ -1,12 +1,18 @@
 
+import 'package:odc_mobile_project/m_chat/business/model/ChatModel.dart';
+import 'package:odc_mobile_project/m_chat/business/model/ChatUsersModel.dart';
+import 'package:odc_mobile_project/m_chat/business/model/Passager.dart';
 import 'package:odc_mobile_project/m_chat/business/model/creerMessageRequete.dart';
 import 'package:odc_mobile_project/m_chat/business/model/messageDetails.dart';
 import 'package:odc_mobile_project/m_chat/business/model/messageGroupe.dart';
+import 'package:odc_mobile_project/m_demande/business/model/Demande.dart';
 
 abstract class MessageNetworkService{
   //Fonctions CRUD
   Future<bool> creerMessage(CreerMessageRequete data);
-  Future<List<MessageGroupe>> recupererListMessageGroupe(int groupId);
+  Future<List<ChatUsersModel>> recupererListMessageGroupe(String token);
+  Future<ChatUsersModel> recupererMessageGroupe(int demandeId);
   Future<bool> supprimerMessageDetail(int messageDetailId);
-  Future<List<MessageDetails>> recupererListMessageDetail(int groupId);
+  Future<List<ChatModel>> recupererListMessageDetail(ChatUsersModel data);
+  Future<Passager> recupererPassagers(ChatUsersModel data);
 }
