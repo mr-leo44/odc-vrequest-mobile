640d0c5aac8c93ee40d85cb5e3c5c6f05fa7ceff
bapmutembs <bapmutembs@gmail.com>
2024-06-24 09:05:49 +0100

    modification des models

--- lib/m_chat/business/model/ChatDetailModel.dart
+++ lib/m_chat/business/model/ChatDetailModel.dart
@@ -1,46 +1,16 @@
+import 'package:odc_mobile_project/m_chat/business/model/ChatUsersModel.dart';
+
 class ChatDetailModel {
-    String avatar;
-    String ticket;
-    String dateDemande;
-    String motif;
-    String lieuDestination;
-    String lieuDepart;
-    String status;
-    String longitude;
-    String latitude;
+    ChatUsersModel chatUsersModel;
 
     ChatDetailModel({
-        this.avatar = "assets/images/avatar.jpeg",
-        required this.ticket,
-        required this.dateDemande,
-        required this.motif,
-        required this.lieuDestination,
-        required this.lieuDepart,
-        required this.status,
-        this.longitude = "",
-        this.latitude = "",
+        required this.chatUsersModel,
     });
 
     ChatDetailModel copyWith({
-        String? avatar,
-        String? ticket,
-        String? dateDemande,
-        String? motif,
-        String? lieuDestination,
-        String? lieuDepart,
-        String? status,
-        String? longitude,
-        String? latitude,
+      ChatUsersModel? chatUsersModel,
     }) => 
         ChatDetailModel(
-            avatar: avatar ?? this.avatar,
-            ticket: ticket ?? this.ticket,
-            dateDemande: dateDemande ?? this.dateDemande,
-            motif: motif ?? this.motif,
-            lieuDestination: lieuDestination ?? this.lieuDestination,
-            lieuDepart: lieuDepart ?? this.lieuDepart,
-            status: status ?? this.status,
-            longitude: longitude ?? this.longitude,
-            latitude: latitude ?? this.latitude,
+            chatUsersModel: chatUsersModel ?? this.chatUsersModel,
         );
 }
--- lib/m_chat/business/model/creerMessageRequete.dart
+++ lib/m_chat/business/model/creerMessageRequete.dart
@@ -4,28 +4,35 @@
 //     final creerMessageRequete = creerMessageRequeteFromJson(jsonString);
 
 import 'package:meta/meta.dart';
+import 'package:odc_mobile_project/m_chat/business/model/DemandeChat.dart';
 import 'dart:convert';
+
+import 'package:odc_mobile_project/m_user/business/model/User.dart';
 
 CreerMessageRequete creerMessageRequeteFromJson(String str) => CreerMessageRequete.fromJson(json.decode(str));
 
 String creerMessageRequeteToJson(CreerMessageRequete data) => json.encode(data.toJson());
 
 class CreerMessageRequete {
-  int demandeId;
+  DemandeChat demande;
+  User user;
   String contenu;
 
   CreerMessageRequete({
-    required this.demandeId,
+    required this.demande,
+    required this.user,
     this.contenu="",
   });
 
   factory CreerMessageRequete.fromJson(Map<String, dynamic> json) => CreerMessageRequete(
-    demandeId: json["demande_id"],
+    demande: json["demande"] ?? null,
+    user: json["user"] ?? null,
     contenu: json["contenu"]?? "",
   );
 
   Map<String, dynamic> toJson() => {
-    "demande_id": demandeId,
+    "demande": demande,
+    "user" : user,
     "contenu": contenu,
   };
 }
