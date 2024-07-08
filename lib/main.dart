7a2dca33f03120a92a75b9250a0aabe259c48b97
salomon <salomonluveto2023@gmail.com>
2024-07-08 11:06:32 +0100

    integration de sembast dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:get_storage/get_storage.dart';
+import 'package:path_provider/path_provider.dart';
+
+
 import 'package:odc_mobile_project/m_chat/business/interactor/chatInteractor.dart';
 import 'package:odc_mobile_project/m_chat/ui/framework/ChatLocalServiceV1.dart';
 import 'package:odc_mobile_project/m_chat/ui/framework/ChatNetworkServiceV1.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+
 
 import 'm_user/business/interactor/UserInteractor.dart';
 import 'm_user/ui/framework/UserLocalServiceImpl.dart';
 import 'm_user/ui/framework/UserNetworkServiceImpl.dart';
 import 'navigation/routers.dart';
+import 'package:path/path.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await GetStorage.init();
-
-  var stockage = GetStorage();
-
+  final appDir = await getApplicationDocumentsDirectory();
+  // await appDir.create(recursive: true);
+  final dbPath = join(appDir.path, "sembast.db");
+  DatabaseFactory dbFactory = databaseFactoryIo;
+  Database db = await dbFactory.openDatabase(dbPath);
   var baseUrl = dotenv.env['BASE_URL'] ?? "";
 
 // module user service implementations
   var userNetworkImpl = UserNetworkServiceImpl(baseUrl);
-  var userLocalImpl = UserLocalServiceImpl(stockage);
+  var userLocalImpl = UserLocalServiceImpl(db);
   var userInteractor=UserInteractor.build(userNetworkImpl, userLocalImpl);
 
   var chatNetworkImpl = ChatNetworkServiceV1(baseUrl);
