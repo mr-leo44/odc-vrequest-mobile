75c7bd9a03c4372ba701743620d003c45727fc66
salomon <salomonluveto2023@gmail.com>
2024-07-08 10:52:59 +0100

    création de la page, du controller et le state pour le compteprofil

---
+++ lib/m_user/ui/pages/compte/CompteProfilPageCtrl.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'CompteProfilPageCtrl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$compteProfilPageCtrlHash() =>
+    r'0b64a3377209be1513acebec6178032b48cfa5fb';
+
+/// See also [CompteProfilPageCtrl].
+@ProviderFor(CompteProfilPageCtrl)
+final compteProfilPageCtrlProvider = AutoDisposeNotifierProvider<
+    CompteProfilPageCtrl, CompteProfilPageState>.internal(
+  CompteProfilPageCtrl.new,
+  name: r'compteProfilPageCtrlProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$compteProfilPageCtrlHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CompteProfilPageCtrl = AutoDisposeNotifier<CompteProfilPageState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
