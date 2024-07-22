074f57de390d419d80ed29115fc352fa86252564
Baptiste Mutemba <bapmutembs@gmail.com>
2024-07-22 16:43:04 +0100

    feat: partager les donnees provenant du socket dans toute l'etendu de l'application

---
+++ lib/shared/ui/SharedCtrl.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'SharedCtrl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sharedCtrlHash() => r'00843f8f9799eb3092fa93bccf8ccd26eb3172e0';
+
+/// See also [SharedCtrl].
+@ProviderFor(SharedCtrl)
+final sharedCtrlProvider = NotifierProvider<SharedCtrl, SharedState>.internal(
+  SharedCtrl.new,
+  name: r'sharedCtrlProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$sharedCtrlHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SharedCtrl = Notifier<SharedState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
