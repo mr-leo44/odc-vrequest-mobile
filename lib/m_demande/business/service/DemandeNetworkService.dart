4eb72413f2497d3e5058ad358f2344c2d2f00b95
salomon <salomonluveto2023@gmail.com>
2024-08-19 14:19:37 +0100

    création de 4 usecases et 4 methode dans DemandeNetworkService et l'ajout dans demandeInteractor : getallDemande, getdDemandeTraite, lastDemande et nombreDemande

--- lib/m_demande/business/service/DemandeNetworkService.dart
+++ lib/m_demande/business/service/DemandeNetworkService.dart
@@ -3,13 +3,17 @@ import 'package:odc_mobile_project/m_demande/business/model/DemandeRequest.dart'
 import '../model/Site.dart';
 
 abstract class DemandeNetworkService {
-  Future<String?> creerDemande(DemandeRequest data, String token);
+  Future<bool?> creerDemande(DemandeRequest data, String token);
 
   Future<String?> annulerDemande(int id, String token);
 
   Future<List<Demande>> listDemande(String token);
 
-  Future<Demande> getDemande(int id, String token);
+  Future<Demande?> getDemande(int id, String token);
 
   Future<List<Site>> listSite(String token);
+  Future<Map<String,dynamic>> nombreDemande(int id);
+  Future<List> lastDemande(int id);
+  Future<List<Demande>> getAllDemande(int id);
+  Future<List<Demande>> getDemandeTraite(int id);
 }
