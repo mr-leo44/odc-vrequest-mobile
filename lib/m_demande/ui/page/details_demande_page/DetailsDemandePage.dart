9ed48de201305a45c9717e9ef2f78893fb47efe5
Foster Mbayi <145870975+77foster@users.noreply.github.com>
2024-08-28 16:43:10 +0100

    creation de la carte pour la page details demande et son utilisation dans la DetailsDemandePage

--- lib/m_demande/ui/page/details_demande_page/DetailsDemandePage.dart
+++ lib/m_demande/ui/page/details_demande_page/DetailsDemandePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:odc_mobile_project/m_demande/ui/page/details_demande_page/DetailsDemandeCtrl.dart';
+import 'package:odc_mobile_project/m_demande/ui/page/details_demande_page/DetailsDemandePageMap.dart';
 import 'package:odc_mobile_project/navigation/routers.dart';
 
 class DetailsDemandePage extends ConsumerStatefulWidget {
@@ -29,9 +30,10 @@ class _DetailsDemandePageState extends ConsumerState<DetailsDemandePage> {
     var state = ref.watch(detailsDemandeCtrlProvider);
     return Scaffold(
       appBar: AppBar(
+        foregroundColor: Colors.white,
         title: Text(
-          "Détails de la demande",
-          style: Theme.of(context).textTheme.titleLarge,
+          "Ma Demande",
+          style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
         actions: [
@@ -45,7 +47,7 @@ class _DetailsDemandePageState extends ConsumerState<DetailsDemandePage> {
                 size: 30,
               )),
         ],
-        backgroundColor: Color(0xFFFF4500),
+        backgroundColor: Color(0xFFFF7900),
       ),
       body: Stack(
         children: [
@@ -67,187 +69,216 @@ class _DetailsDemandePageState extends ConsumerState<DetailsDemandePage> {
 
     print("demande ${demande?.motif}");
 
-    return Column(
-      children: [
-        Container(
-          width: double.infinity,
-          color: Color(0xFFFF4500),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.00),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                Text(
+                  "${dateDeplacement!.day.toString().padLeft(2, '0')}-${dateDeplacement.month.toString().padLeft(2, '0')}-${dateDeplacement.year.toString().padLeft(4, '0')}",
+                  style:
+                      TextStyle(fontSize: 22.00, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Date de la course",
+                  style: TextStyle(fontSize: 20.00),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Text("Carte"),
+              Container(
+                child: DetailsDemandePageMap(
+                  demande: demande,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.00),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset(
-                      "assets/images/avatar.jpeg",
-                      height: 150,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${demande!.initiateur!.prenom} ${demande.initiateur!.nom}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20.0),
+                            ),
+                            Text("${demande.ticket}")
+                          ],
+                        ),
+                        Image.asset(
+                          "images/voiture.png",
+                          width: 50,
+                        )
+                      ],
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: demande.status != '1' ? null : () {},
+                      label: Text("Envoyer un message"),
+                      icon: Icon(Icons.send),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFFF7900),
+                          foregroundColor: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Détails de la demande",
+                      style: TextStyle(
+                          fontSize: 18.00, fontWeight: FontWeight.bold),
+                    ),
+                    Divider(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          "${demande?.initiateur!.nom} ${demande?.initiateur!.prenom}",
+                          "${date!.day.toString().padLeft(2, '0')}-${date.month.toString().padLeft(2, '0')}-${date.year.toString().padLeft(4, '0')}\n${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}",
                           style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500),
-                        ),
-                        /*Text(
-                          "${_jourEnLettre(date!.weekday)} ",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w400, fontSize: 20),
+                              fontSize: 16.00, fontWeight: FontWeight.w400),
                         ),
                         Text(
-                          "${date!.day.toString().padLeft(2, '0')}-${date.month.toString().padLeft(2, '0')}-${date.year.toString().padLeft(4, '0')}",
+                          "Date de la demande",
                           style: TextStyle(
-                              fontWeight: FontWeight.w400, fontSize: 20),
-                        ),*/
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            demande.motif,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 5,
+                            style: TextStyle(
+                                fontSize: 16.00, fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        Text(
+                          "Motif",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "${demande.nbrePassagers} passagers",
+                      style: TextStyle(
+                          fontSize: 16.00, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "${_jourEnLettre(dateDeplacement!.weekday)}\n${dateDeplacement.day.toString().padLeft(2, '0')}-${dateDeplacement.month.toString().padLeft(2, '0')}-${dateDeplacement.year.toString().padLeft(4, '0')}\n${dateDeplacement.hour.toString().padLeft(2, '0')}:${dateDeplacement.minute.toString().padLeft(2, '0')}",
+                          style: TextStyle(
+                              fontSize: 16.00, fontWeight: FontWeight.w400),
+                        ),
+                        Text(
+                          "Date de la course",
+                          style: TextStyle(fontSize: 16.00),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("${_getStatus(demande.status)}", style: TextStyle(
+                          color: demande.status != '1' ? Colors.grey : Colors.green, fontWeight: FontWeight.w400, fontSize: 16.00
+                        ),),
+                        Text("status", style: TextStyle(fontSize: 16.00),)
                       ],
                     )
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      demande!.status == "1"
-                          ? "Discuter avec le chauffeur"
-                          : "En attente du début de la course",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.chat,
-                        color:
-                            demande!.status == "1" ? Colors.black : Colors.grey,
-                        size: 50,
-                      ),
-                      onPressed: demande!.status == "1" ? () {} : null,
-                    ),
-                  ],
-                )
-              ],
+              ),
             ),
           ),
-        ),
-        Container(
-          width: screenWidth * 0.85,
-          padding: EdgeInsets.all(8.0),
-          margin: EdgeInsets.only(top: 35),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: demande!.status != "1" ? Color(0xFFFFC107) : Colors.green,
-              // Couleur de la bordure
-              width: 3.0, // Épaisseur de la bordure
-            ),
-            borderRadius: BorderRadius.circular(8.0), // Rayon des coins
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "${demande?.motif}",
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-              Text(
-                _getStatus("${demande?.status}"),
-                textAlign: TextAlign.end,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.all(8.0),
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Color(0x809E9E9E),
-                  borderRadius: BorderRadius.circular(8.0), // Rayon des coins
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.car_repair, size: 35),
-                    Text(
-                      "${_jourEnLettre(dateDeplacement!.weekday)} ${dateDeplacement.day.toString().padLeft(2, '0')}-${dateDeplacement.month.toString().padLeft(2, '0')}-${dateDeplacement.year.toString().padLeft(4, '0')} à ${dateDeplacement.hour.toString().padLeft(2, '0')}:${dateDeplacement.minute.toString().padLeft(2, '0')}",
-                      style: TextStyle(
-                          fontSize: 15.00, fontWeight: FontWeight.w400),
-                    ),
-                    Text(
-                      "${demande?.nbrePassagers} passagers",
-                      style: TextStyle(fontSize: 12),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                margin: EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    _lieuInfo(demande?.lieuDepart, "Lieu de départ"),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 6.0, horizontal: 20),
-                      child: Divider(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    _lieuInfo(demande?.destination, "Déstination"),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: demande.status != '1' ? () {
-                      var ctrl = ref.read(detailsDemandeCtrlProvider.notifier);
-                      ctrl.annulerDemande(widget.id);
-                      context.goNamed(Urls.listeDemandes.name);
-                    } : null,
-                    child: Text("Annuler"),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor:
-                          demande.status != '1' ? Colors.black : Colors.grey,
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
+          Padding(
+            padding: EdgeInsets.only(left: 16.5, right: 16.5, bottom: 10),
+            child: Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: demande.status != '1'
+                    ? () {
+                        var ctrl =
+                            ref.read(detailsDemandeCtrlProvider.notifier);
+                        ctrl.annulerDemande(widget.id);
+                        context.goNamed(Urls.listeDemandes.name);
+                      }
+                    : null,
+                child: Text("Annuler"),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ],
-              )
-            ],
+                ),
+              ),
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
-  }
-
-  String _getDayOfWeek(DateTime date) {
-    final formatter = DateFormat('EEEE');
-    return formatter.format(date);
   }
 
   String _getStatus(String status) {
