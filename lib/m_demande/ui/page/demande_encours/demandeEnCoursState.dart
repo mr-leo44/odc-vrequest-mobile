e229695849a8d6367887d0214fbaf9ba1839fa21
Foster Mbayi <145870975+77foster@users.noreply.github.com>
2024-09-02 13:27:43 +0100

    ajout des champs listDemandeSearch, notFound et visible dans demandeEnCoursState et DemandeTraiteState

--- lib/m_demande/ui/page/demande_encours/demandeEnCoursState.dart
+++ lib/m_demande/ui/page/demande_encours/demandeEnCoursState.dart
@@ -6,31 +6,40 @@ class DemandeEnCoursState {
   int nbrDemande;
   User? user;
   List<Demande> demande;
+  List<Demande> listDemandesSearch;
   bool isLoading;
   bool isEmpty;
   bool visible;
+  bool notFound;
 
-  DemandeEnCoursState(
-      {this.isLoading = false,
-      this.isEmpty = false,
-      this.visible = false,
-      this.nbrDemande = 0,
-      this.user = null,
-      this.demande = const []});
+  DemandeEnCoursState({
+    this.isLoading = false,
+    this.isEmpty = false,
+    this.visible = false,
+    this.notFound = false,
+    this.nbrDemande = 0,
+    this.user = null,
+    this.demande = const [],
+    this.listDemandesSearch = const [],
+  });
 
   DemandeEnCoursState copyWith(
           {int? nbrDemande,
           User? user,
           List<Demande>? demande,
+          List<Demande>? listDemandesSearch,
           bool? isLoading,
           bool? visible,
+          bool? notFound,
           bool,
           isEmpty}) =>
       DemandeEnCoursState(
           nbrDemande: nbrDemande ?? this.nbrDemande,
           user: user ?? this.user,
           demande: demande ?? this.demande,
+          listDemandesSearch: listDemandesSearch ?? this.listDemandesSearch,
           isLoading: isLoading ?? this.isLoading,
           visible: visible ?? this.visible,
+          notFound: notFound ?? this.notFound,
           isEmpty: isEmpty ?? this.isEmpty);
 }
