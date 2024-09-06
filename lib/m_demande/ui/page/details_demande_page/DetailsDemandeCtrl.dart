5c8e2c7b4f123740cd41c864a4c146c3735975cd
Baptiste Mutemba <bapmutembs@gmail.com>
2024-09-06 12:34:06 +0100

    feat: ajouter la methode pour recuperer l'id du chat dans le detail de la demande

--- lib/m_demande/ui/page/details_demande_page/DetailsDemandeCtrl.dart
+++ lib/m_demande/ui/page/details_demande_page/DetailsDemandeCtrl.dart
@@ -1,6 +1,9 @@
+import 'package:odc_mobile_project/m_chat/business/interactor/chatInteractor.dart';
+import 'package:odc_mobile_project/m_chat/business/model/ChatUsersModel.dart';
 import 'package:odc_mobile_project/m_demande/business/interactor/demandeInteractor.dart';
 import 'package:odc_mobile_project/m_demande/business/model/Demande.dart';
 import 'package:odc_mobile_project/m_demande/ui/page/details_demande_page/DetailsDemandeState.dart';
+import 'package:odc_mobile_project/m_user/business/interactor/UserInteractor.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'DetailsDemandeCtrl.g.dart';
@@ -28,4 +31,19 @@ class DetailsDemandeCtrl extends _$DetailsDemandeCtrl {
     var useCase = ref.watch(demandeInteractorProvider).annulerDemandeUseCase;
     var res = await useCase.run(id);
   }
+
+  Future<String> getToken() async{
+    var usecase =  ref.watch(userInteractorProvider).getTokenUseCase;
+    var res = await usecase.run();
+    return Future.value(res);
+  }
+
+  void getMessages(int demandeId) async {
+    state = state.copyWith(isLoading: true, chatsUsers: null);
+    var token = await getToken(); 
+    var interactor = ref.watch(chatInteractorProvider);
+    ChatUsersModel res = await interactor.recupererMessageGroupeUseCase.run(demandeId,token);
+    state = state.copyWith(isLoading: false, chatsUsers: res);
+  }
+
 }
